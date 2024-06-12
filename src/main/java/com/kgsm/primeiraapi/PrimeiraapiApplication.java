a0b6add74907186bf73d0dd45c8376360bc9760a
Kauã <kauamoreira90@gmail.com>
2024-06-12 01:09:55 -0300

    Projeto API com Spring

---
+++ src/main/java/com/kgsm/primeiraapi/PrimeiraapiApplication.java
@@ -0,0 +1,13 @@
+package com.kgsm.primeiraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgsm/primeiraapi/PrimeiraapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgsm.primeiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
